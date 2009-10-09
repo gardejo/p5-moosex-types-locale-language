0e0a06403998c76cad1cd94aeb6218ffacb17eeb
gardejo <moriya@ermitejo.com>
2009-10-10 02:53:08 +0900

    added INCOMPATIBILITIES and BUGS AND LIMITATIONS sections to POD

--- lib/MooseX/Types/Locale/Language.pm
+++ lib/MooseX/Types/Locale/Language.pm
@@ -178,6 +178,57 @@ For example, C<'JAPANESE'> will convert to C<'Japanese'>.
 
 =back
 
+=head1 INCOMPATIBILITIES
+
+None reported.
+
+=head1 BUGS AND LIMITATIONS
+
+No bugs have been reported.
+
+=head2 Making suggestions and reporting bugs
+
+Please report any found bugs, feature requests, and ideas for improvements
+to C<bug-moosex-types-locale-language at rt.cpan.org>,
+or through the web interface
+at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MooseX-Types-Locale-Language>.
+I will be notified, and then you'll automatically be notified of progress
+on your bugs/requests as I make changes.
+
+When reporting bugs, if possible,
+please add as small a sample as you can make of the code
+that produces the bug.
+And of course, suggestions and patches are welcome.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the C<perldoc> command.
+
+    perldoc MooseX::Types::Locale::Language
+
+You can also look for information at:
+
+=over 4
+
+=item RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=MooseX-Types-Locale-Language>
+
+=item AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/MooseX-Types-Locale-Language>
+
+=item Search CPAN
+
+L<http://search.cpan.org/dist/MooseX-Types-Locale-Language>
+
+=item CPAN Ratings
+
+L<http://cpanratings.perl.org/d/MooseX-Types-Locale-Language>
+
+=back
+
 =head1 VERSION CONTROL
 
 This module is maintained using git.
